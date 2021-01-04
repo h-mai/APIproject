7941ced6784738e6a4841f8735f415eb0714f195
Lee Kiri <leekiritempleton@gmail.com>
2021-01-04 22:17:45 +1100

    CSS made responsive and navbar fixed

--- assets/carousel.js
+++ assets/carousel.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Carousel.init(elems, {fullWidth: true, indicators: true});
     carouselInterval = setInterval(function(){
         instances[0].next();
-    }, 3000);
+    }, 5000);
   });
-
-  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
