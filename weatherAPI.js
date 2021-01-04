209e867d975e95b3132a1dd7940781b0bf3f8a59
avpizarro <avpizarro@live.com>
2021-01-04 22:07:59 +1100

    Update weatherAPI

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,7 +1,9 @@
 
 //Daily Weather Forecast
 
-var queryURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey "?apikey=" + apikey + "&language=en-us&details=false&metric=true"
+var queryURL= "https://dataservice.accuweather.com/forecasts/v1/daily/1day/26216?apikey=f6QbiHKwOvouxHGTPJ3GHffRRblmTZBG&language=en-us&details=false&metric=true"
+
+// var queryURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=" + apikey + "&language=en-us&details=false&metric=true"
 
 //Accuweather API Key
 var apikey = "f6QbiHKwOvouxHGTPJ3GHffRRblmTZBG";
@@ -9,39 +11,41 @@ var apikey = "f6QbiHKwOvouxHGTPJ3GHffRRblmTZBG";
 //Accuweather Location Key for Melbourne City 
 var locationKey = "26216";
 
-var minTemp = [];	
-var maxTemp = [];	
-var currentDate = [];
-var weatherText = [];
-var weatherIcon = [];
-var forecastDay = 1
+var todaysForecast = {};
 
 //fetching Accuweather Daily Weather Data
 async function fetchWeather () {
 
-    await fetch(queryURL)
-    .then(function(resp) { return resp.json() })
-    .then(function(data) {
-      console.log(data);
-    })
-    .catch(function() {
-
-    });
-
-}
-
-var dailyForecast = function(data) {
-
-    for (var i = 0; i < forecastDay; i++) {
-        currentDate[i] = data.DailyForecasts[i].Date;
-        minTemp[i] = data.DailyForecasts[i].Temperature.Minimum.Value;
-        maxTemp[i] = data.DailyForecasts[i].Temperature.Maximum.Value;
-        weatherText[i] = data.DailyForecasts[i].Day.IconPhrase;
-        weatherIcon[i] = data.DailyForecasts[i].Day.Icon;
-        
+    var resp = await fetch(queryURL)
+    if (!resp.ok) {
+        throw Error("ERROR");
     }
-console.log(dailyForecast)
-};
+    resp = await resp.json()
+
+    console.log(resp);
+
+    console.log(resp.DailyForecasts[0].Date)
+    console.log(resp.DailyForecasts[0].Temperature.Minimum.Value)
+    console.log(resp.DailyForecasts[0].Temperature.Maximum.Value)
+    console.log(resp.DailyForecasts[0].Day.IconPhrase)
+    console.log(resp.DailyForecasts[0].Day.Icon)
+
+    // src = "assets/" + resp.DailyForecasts[0].Day.Icon + ".png"
+
+
+    todaysForecast = {
+        currentDate :  resp.DailyForecasts[0].Date,
+        minTemp : resp.DailyForecasts[0].Temperature.Minimum.Value,
+        maxTemp : resp.DailyForecasts[0].Temperature.Maximum.Value,
+        weatherText : resp.DailyForecasts[0].Day.IconPhrase,
+        weatherIcon : resp.DailyForecasts[0].Day.Icon, 
+    };
+    
+    console.log(todaysForecast)
+}
+    
+fetchWeather();
+
 
 
 
