056651f7f82691688b541287ae09142644ec42a9
Lee Kiri <leekiritempleton@gmail.com>
2021-01-05 21:42:51 +1100

    CSS and HTML tweaked

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {fullWidth: true, indicators: true});
+    if(instances.length>0) {
     carouselInterval = setInterval(function(){
-        instances[0].next();
+      instances[0].next();
     }, 5000);
+  }
   });
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
