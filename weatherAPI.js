e28552a9e4ebec1ed29ae83130faa2be4a7d9d8b
pfotis <69961508+pfotis@users.noreply.github.com>
2021-01-07 22:44:32 +1100

    add new javascript file for the fovarite page and set up local storage save abd remove

--- weatherAPI.js
+++ weatherAPI.js
@@ -3,7 +3,7 @@
 
 //Accuweather API Key and Location key for Melbourne City
 var locationKey = "26216";
-var apikey = "f6QbiHKwOvouxHGTPJ3GHffRRblmTZBG";
+var apikey = "";
 var queryURL = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=" + apikey + "&language=en-us&details=false&metric=true"
 
 //DOM for storing weather data
